b980a7c6ca35c11449125f9c20611e2f3811afbb
saravananaztf <149578364+saravananaztf@users.noreply.github.com>
2023-11-24 23:11:29 +1100

    Update c2-resourcegroup.tf
    Update tags

--- terraform-manifest/c2-resourcegroup.tf
+++ terraform-manifest/c2-resourcegroup.tf
@@ -2,4 +2,5 @@ resource "azurerm_resource_group" "myrg1" {
   name = "${local.resourcename-prefix}-myrg-${random_string.myrandom.id}"
   location = var.resource_group_location
   tags = local.common_tags
+  env = prod
 }
