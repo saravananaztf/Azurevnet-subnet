fd7b86c7b3ad4b0c3ec0cfc07892d274a1e5b6ae
saravanan <saravananmohan2410@gmail.com>
2023-11-24 23:23:35 +1100

    Tagremovd

--- terraform-manifest/c7-vnet.tf
+++ terraform-manifest/c7-vnet.tf
@@ -3,6 +3,6 @@ resource "azurerm_virtual_network" "myvnet" {
     location = var.resource_group_location
     address_space = var.vnetaddresspace
     resource_group_name = azurerm_resource_group.myrg1.name
-    tags = local.common_tags
+    
     depends_on = [ azurerm_resource_group.myrg1 ]
 }
